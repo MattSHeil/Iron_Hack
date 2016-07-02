c067d7d3a43293a3606f1bdd559944442f850191
mattsheil <matheus.heil001@mymdc.net>
2016-07-02 11:22:44 -0400

    Added method to create short link

--- link_shortner/app/models/shortner.rb
+++ link_shortner/app/models/shortner.rb
@@ -1,2 +1,9 @@
 class Shortner < ActiveRecord::Base
+
+	ALPHABET = [('a'..'z').to_a, ('A'..'Z').to_a].flatten
+
+	def self.generate_shorturl(link_legth)
+		ALPHABET.sample(link_length).join
+	end
+
 end
