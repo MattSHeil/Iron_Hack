083d5f72f520f2e689b8647aaeaf701c5d32737d
mattsheil <matheus.heil001@mymdc.net>
2016-07-02 11:07:55 -0400

    Created shorner controller logic to find link by it's short url

--- link_shortner/app/controllers/shortner_controller.rb
+++ link_shortner/app/controllers/shortner_controller.rb
@@ -1,2 +1,11 @@
 class ShortnerController < ApplicationController
+
+	def index
+		link = Shortner.find_by(shorturl: params[:shorturl])
+
+		if link 
+			redirect_to link.original
+		end
+	end
+
 end
