39f7591a86f72a6469f9661d6864cc42d1bab4e0
mattsheil <matheus.heil001@mymdc.net>
2016-07-09 10:53:09 -0400

    DB migrated and seeds

--- leConcertHub/db/seeds.rb
+++ leConcertHub/db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Concert.create!(artist:"Slipknot", venue:"AAA Arena", city:"Miami", 
+	date: Time.new(2016, 07, 30, 19), price: 40.00, 
+	description: "Your bones don't break, mine do. That's clear.
+	Your cells react to bacteria and viruses differently than mine.
+	You don't get sick, I do. That's also clear. But for some reason,
+	you and I react the exact same way to water. We swallow it too fast,
+	we choke. We get some in our lungs, we drown. However unreal it may seem,
+	we are connected, you and I. We're on the same curve, just on opposite ends." )
+Concert.create!(artist:"Lamb of God", venue:"Ft Lauderdale Pavillion", city:"Ft Lauderdale",
+	date: Time.new(2016, 07, 20, 19), price: 25.00, 
+	description: "Your bones don't break, mine do. That's clear.
+	Your cells react to bacteria and viruses differently than mine.
+	You don't get sick, I do. That's also clear. But for some reason,
+	you and I react the exact same way to water. We swallow it too fast,
+	we choke. We get some in our lungs, we drown. However unreal it may seem,
+	we are connected, you and I. We're on the same curve, just on opposite ends." )
+Concert.create!(artist:"Muse", venue:"Some Venue", city:"Jacksonville",
+	date: Time.new(2016, 07, 10, 19), price: 50.00, 
+	description: "Your bones don't break, mine do. That's clear.
+	Your cells react to bacteria and viruses differently than mine.
+	You don't get sick, I do. That's also clear. But for some reason,
+	you and I react the exact same way to water. We swallow it too fast,
+	we choke. We get some in our lungs, we drown. However unreal it may seem,
+	we are connected, you and I. We're on the same curve, just on opposite ends." )
+Concert.create!(artist:"Linking Park", venue:"SomeOther Venue", city:"Tampa",
+	date: Time.new(2016, 07, 4, 19), price: 20.00, 
+	description: "Your bones don't break, mine do. That's clear.
+	Your cells react to bacteria and viruses differently than mine.
+	You don't get sick, I do. That's also clear. But for some reason,
+	you and I react the exact same way to water. We swallow it too fast,
+	we choke. We get some in our lungs, we drown. However unreal it may seem,
+	we are connected, you and I. We're on the same curve, just on opposite ends." )
+Concert.create!(artist:"AC DC", venue:"SunLife Stadium", city:"Miami",
+	date: Time.new(2016, 07, 27, 19), price: 60.00, 
+	description: "Your bones don't break, mine do. That's clear.
+	Your cells react to bacteria and viruses differently than mine.
+	You don't get sick, I do. That's also clear. But for some reason,
+	you and I react the exact same way to water. We swallow it too fast,
+	we choke. We get some in our lungs, we drown. However unreal it may seem,
+	we are connected, you and I. We're on the same curve, just on opposite ends." )
