551812f1a118de6f1d0c5ea7576783b61125e1c9
mattsheil <matheus.heil001@mymdc.net>
2016-08-09 21:36:19 -0400

    Started api exercise generated resources for sandwich and ingredients, actions index, show, create and delete, fully functional

---
+++ Sandwichr/app/controllers/ingredients_controller.rb
@@ -0,0 +1,42 @@
+class IngredientsController < ApplicationController
+
+	protect_from_forgery with: :null_session
+
+	def index
+		ingredients = Ingredient.all
+		render json: ingredients
+	end
+
+	def create
+		ingredient = Ingredient.create(ingredientsParam)
+		render json: ingredient
+	end
+
+	def show
+		ingredient = Ingredient.find_by(id: params[:id])
+		unless ingredient
+			render json: {error: "ingredient dont exist"},
+				status: 404
+			return
+		end
+		render json: ingredient
+	end
+
+	def destroy
+		ingredient = Ingredient.find_by(id: params[:id])
+		unless ingredient
+			render json: {error: "ingredient dont exist"},
+				status: 404
+			return
+		end
+		ingredient.destroy
+		render json: ingredient
+	end
+
+	private
+
+	def ingredientsParam
+		params.require(:ingredients).permit(:name, :calories)
+	end
+
+end
---
+++ Sandwichr/app/controllers/sandwiches_controller.rb
@@ -0,0 +1,52 @@
+class SandwichesController < ApplicationController
+
+	protect_from_forgery with: :null_session
+
+	def index
+		sandwiches = Sandwich.all
+		render json: sandwiches
+	end
+
+	def create
+		sandwich = Sandwich.create(sandwichParams)
+		render json: sandwich 
+	end
+
+	def show
+		sandwich = Sandwich.find_by(id: params[:id])
+		unless sandwich
+			render json: {error: "sandwich dont exist"},
+				status: 404
+			return
+		end
+		render json: sandwich
+	end
+
+	def update 
+		sandwich = Sandwich.find_by(id: params[:id])
+		unless sandwich
+			render json: {error: "sandwich dont exist"},
+				status: 404
+			return
+		end
+		sandwich.update(sandwichParams)
+		render json: sandwich
+	end
+
+	def destroy
+		sandwich = Sandwich.find_by(id: params[:id])
+		unless sandwich
+			render json: {error: "sandwich dont exist"},
+				status: 404
+			return
+		end
+		sandwich.destroy
+		render json: sandwich
+	end
+
+	private
+
+	def sandwichParams
+		params.require(:sandwich).permit(:name, :bread_type)
+	end
+end
