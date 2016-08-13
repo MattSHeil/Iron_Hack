fd97e87fc8f3714521959bdf12a5de4923012aa7
Mnatherr <matheus.heil001@mymdc.net>
2016-08-13 16:38:26 -0400

    Migrations for the 3 models were done

--- video-games-tournament/db/migrate/20160813203456_create_registrations.rb
+++ video-games-tournament/db/migrate/20160813203456_create_registrations.rb
@@ -1,6 +1,8 @@
 class CreateRegistrations < ActiveRecord::Migration
   def change
     create_table :registrations do |t|
+      t.integer :tournament_id
+      t.integer :player_id
 
       t.timestamps null: false
     end
